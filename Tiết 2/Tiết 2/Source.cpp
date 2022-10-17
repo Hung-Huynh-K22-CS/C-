786f625655aaf2fc8690f1b76220ee6b1937fbbc
Hung-Huynh-K22-CS <115800533+Hung-Huynh-K22-CS@users.noreply.github.com>
2022-10-17 12:42:47 +0700

    Add files via upload

---
+++ Tiết 2/Tiết 2/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+int main() {
+	double a, b, c;
+	
+	cout << "Nhap so thu 1: ";
+	cin >> a;
+	cout << "Nhap so thu 2: ";
+	cin >> b;
+	cout << "Nhap so thu 3: ";
+	cin >> c;
+	cout << "Average 3 num: " << (a + b + c) / 3 << endl;
+
+	int main(); {
+		cout << "a + b = " << a + b << endl;
+		cout << "a - b = " << a - b << endl;
+		cout << "a * b = " << a * b << endl;
+		cout << "a / b = " << a / b << endl;
+	}
+	int main(); {
+		double  per;
+		per = a / b * 100;
+		cout << per << "%";
+
+	}
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
